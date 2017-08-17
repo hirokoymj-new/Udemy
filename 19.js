68c4b411bd91dd155604db641119b415e73aa058
hirokoymj <hiroko@hirokoymj.com>
2017-08-16 21:18:59 -0700

    updated javascript.md

--- 19.js
+++ 19.js
@@ -1,6 +1,5 @@
-# Constroctor OO Pattern - Part2
+"use strict";
 
-```js
 function Person(first_name, last_name){
     this.first_name = first_name;
     this.last_name = last_name;
@@ -8,8 +7,3 @@ function Person(first_name, last_name){
 Person.prototype.full_name = function () {
     return this.first_name +  " " + this.last_name;
 }
-
-var dude = new Person("asim", "hussain");
-dude.first_name = "Moo";
-console.log(dude.full_name());
-```
