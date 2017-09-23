37ec516853a0aacdb6dfd87f8cbb9e5873bf795b
hirokoymj <hiroko@hirokoymj.com>
2017-09-23 13:06:21 -0700

    updated Read me file

---
+++ PHP/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hiroko
+ * Date: 9/23/17
+ * Time: 12:56 PM
+ */
+
+
+$input = array("red", "green", "blue", "yellow");
+array_splice($input, 1, 1);
+print_r($input);
+
+?>
